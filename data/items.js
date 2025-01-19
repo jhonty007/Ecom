6dc676e77c52e6ed41504a007dda97dca1cc939e
Jhonty <119353020+jhonty007@users.noreply.github.com>
2025-01-19 22:27:50 +0530

    Add files via upload

---
+++ data/items.js
@@ -0,0 +1,30 @@
+const items = [
+    {
+        id: '001',
+        image: 'images/1.jpg',
+        item_name: 'Inkspire',
+        original_price: 45,
+        current_price: 20,
+        discount_percentage: 56,
+        return_period: 14,
+        delivery_date: '22-01-2024',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    },
+    {
+        id: '002',
+        image: 'images/2.jpg',
+        item_name: 'Inkwella',
+        original_price: 50,
+        current_price: 30,
+        discount_percentage: 40,
+        return_period: 14,
+        delivery_date: '22-01-2024',
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
+    }
+];
